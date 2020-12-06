e97af7f1afd49c2a40c0864044cb3827ccc753a6
mfahmirukman <muhammad.fahmi1893@gmail.com>
2020-12-06 18:03:01 +0700

    fixing main.go

--- main.go
+++ main.go
@@ -1,27 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"./handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
+	l := log.New(os.Stdout, "go-micro", log.LstdFlags)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         "localhost:9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		l.Println("Starting server on port 9090")
+
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(rw, "Ooops", http.StatusBadRequest)
-			return
+			l.Printf("Error starting server: %s\n", err)
+			os.Exit(1)
 		}
+	}()
 
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Kill)
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World")
-	})
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
 
-	http.ListenAndServe("localhost:9090", nil)
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(ctx)
 }
